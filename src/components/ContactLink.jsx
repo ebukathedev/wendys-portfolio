c2f501c659c9974d3bf575d9492345e59798b124
Ebuka Ejiofor <ebuskelele@gmail.com>
2023-01-14 01:16:57 -0800

    add font class

--- src/components/ContactLink.jsx
+++ src/components/ContactLink.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const ContactLink = ({ children, href, name }) => {
 	return (
-		<div className="flex items-center space-x-4">
+		<div className="flex items-center space-x-4 font-raleway">
 			{children}
-			<a href={href} className=" font-normal text-lg font-raleway">
+			<a href={href} className=" font-normal text-sm sm:text-lg">
 				{name}
 			</a>
 		</div>
